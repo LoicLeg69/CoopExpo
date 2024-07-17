8c7b943d25531dbd849382b47559d642c77012f0
Loic legoff <loic95280@gmail.com>
2024-07-17 12:41:02 +0200

    install argon2

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     job VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
+    mail VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
 );
 
